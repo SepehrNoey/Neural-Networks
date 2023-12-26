644c0fde5c5501ba3e9afe9d095b1b7a605ae27a
SepehrNoey <sepehr.nk.81@gmail.com>
2023-12-26 23:33:22 +0330

    feat: implemented losses

--- rsdl/losses/loss_functions.py
+++ rsdl/losses/loss_functions.py
@@ -2,11 +2,19 @@ from rsdl import Tensor
 
 def MeanSquaredError(preds: Tensor, actual: Tensor):
     # TODO : implement mean squared error
-    return None
+    err = actual - preds
+    return (err ** 2).sum() / len(actual.data)
 
 def CategoricalCrossEntropy(preds: Tensor, actual: Tensor):
     # TODO : imlement categorical cross entropy 
-    return None
+    exps = preds.exp()
+    sum_exps = exps.sum()
+    softmaxed_preds = sum_exps * Tensor(data=(1 / sum_exps.data),
+        requires_grad=sum_exps.requires_grad, depends_on=sum_exps.depends_on)
+    log_softmaxed_preds = softmaxed_preds.log()
+    cce_vector = - (actual * log_softmaxed_preds)
+    cce = - (cce_vector.sum())
+    return cce
 
 
 
